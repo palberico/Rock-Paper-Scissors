aee2fb0eb6a2ecf7c4f30bd4a294d14f3ab71de8
Pete <palberico@yahoo.com>
2017-11-15 09:23:42 -0700

    RPS assignment

---
+++ java/java.js
@@ -0,0 +1,21 @@
+var rockButton = document.getElementById("rock")
+var paperButton = document.getElementById("paper")
+var scissorButton = document.getElementById("scissors")
+
+var computerChoice = ["Rock", "Paper", "Scissor"]
+
+
+rockButton.addEventListener("click", function(){
+    // winner("rock")
+    console.log("pizza")
+})
+
+paperButton.addEventListener("click", function(){
+    // winner("paper")
+    console.log("fries")
+})
+
+scissorButton.addEventListener("click", function(){
+    // winner("scissor")
+    console.log("burger")
+})
